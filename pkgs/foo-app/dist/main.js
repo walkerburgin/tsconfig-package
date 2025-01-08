6b2a99a58822250f204173b013d842153497facb
Walker Burgin <wburgin@anduril.com>
2025-01-08 14:50:43 -0800

    Replace `dist` directories with symlinks into `bazel-bin`

--- pkgs/foo-app/dist/main.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
-const el = document.createElement("div");
-document.body.appendChild(el);
-const root = createRoot(el);
-root.render(/*#__PURE__*/ _jsx(App, {}));
